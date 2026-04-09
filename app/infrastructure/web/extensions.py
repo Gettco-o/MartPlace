9feb8dfcc2c54ff19d071b5a86b146448e1659a8
Gettco-o <ecovenant74@gmail.com>
2026-04-09 21:51:55 +0100

    readme file | docs label

--- app/infrastructure/web/extensions.py
+++ app/infrastructure/web/extensions.py
@@ -1,7 +1,12 @@
 from app.infrastructure.db import Database
 from app.infrastructure.event_bus import SimpleEventBus
-from quart_schema import QuartSchema
+from quart_schema import QuartSchema,Info
 
 db = Database()
 event_bus = SimpleEventBus()
-qs = QuartSchema()
+qs = QuartSchema(
+      info=Info(
+            title="MartPlace",
+            version="1.0.0",
+      )
+)
