54419b7d6473abff0857766f340d07b8f503d720
Gettco-o <ecovenant74@gmail.com>
2026-07-23 15:03:46 +0100

    aync event bus

--- app/infrastructure/event_bus.py
+++ app/infrastructure/event_bus.py
@@ -7,6 +7,7 @@ from blinker import NamedSignal, Signal, signal
 
 from concurrent.futures import ThreadPoolExecutor
 
+#simple event bus implementation
 """ 
 class ISimpleEventBus(EventBus):
     def __init__(self) -> None:
@@ -27,7 +28,7 @@ class ISimpleEventBus(EventBus):
                     self._logger.exception("Error while handling event %s", type(event))
 
  """
-
+# event bus with blinker library
 """
 class SimpleEventBus(EventBus):
     def __init__(self):
@@ -82,3 +83,36 @@ class SimpleEventBus(EventBus):
                 "Error while handling event %s",
                 type(event).__name__,
             ) 
+
+# TODO: async event bus implementation using asyncio and async signals
+"""
+class AsyncEventBus(EventBus):
+    def __init__(self):
+        self._emitter: dict[type, Signal] = {}
+        self._logger = logging.getLogger(__name__)
+
+    def register(self, event_type: type, handler: Callable[[Any], None]):
+        emitter_obj = self._emitter.get(event_type)
+        if emitter_obj is None:
+            emitter_obj = Signal(event_type.__name__)
+            self._emitter[event_type] = emitter_obj
+        emitter_obj.connect(handler, weak=False)
+        
+        
+    async def publish(self, events: Iterable[Any]):
+        for event in events:
+            event_type = type(event)
+            emitter_obj = self._emitter.get(event_type)
+            if emitter_obj:
+                await self._safe_send(emitter_obj, event)
+
+    async def _safe_send(self, emitter_obj: Signal, event):
+        try:
+            emitter_obj.send(event)
+        except Exception:
+            self._logger.exception(
+                "Error while handling event %s",
+                type(event).__name__,
+            )
+
+"""
