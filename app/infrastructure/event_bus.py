b0ee3b7d6d0a8ac66246e7c3436a5275a594ab9b
Gettco-o <ecovenant74@gmail.com>
2026-07-25 00:32:03 +0100

    hybrid approach to handle sync handlers, async handlers, and CPU bound handlers

--- app/infrastructure/event_bus.py
+++ app/infrastructure/event_bus.py
@@ -1,11 +1,14 @@
+import inspect
 import logging
 from typing import Any, Iterable, Callable
 
 from app.interfaces.event_bus import EventBus
 
-from blinker import NamedSignal, Signal, signal 
+from blinker import Signal
 
 from concurrent.futures import ThreadPoolExecutor
+
+from concurrent.futures import ProcessPoolExecutor
 
 import asyncio
 
@@ -90,6 +93,7 @@ class SimpleEventBus(EventBus):
 
 # events with threadpool and dedicated workers for handlers (not just events)
 # so each handler is dedicated to a worker and can run concurrently with other handlers within same event type
+"""
 class SimpleEventBus(EventBus):
     def __init__(self):
         self._emitter: dict[type, Signal] = {}
@@ -123,6 +127,67 @@ class SimpleEventBus(EventBus):
     def _safe_handle(self, handler, event):
         try:
             handler(event)
+        except Exception:
+            self._logger.exception(
+                "Error handling event %s",
+                type(event).__name__
+            )
+"""
+# event bus that makes use of a hybrid approach to handle sync handlers, async handlers, and CPU bound handlers
+cpu_bound_handlers = []
+class SimpleEventBus(EventBus):
+    def __init__(self):
+        self._emitter: dict[type, Signal] = {}
+        self._logger = logging.getLogger(__name__)
+        self._thread_executor = ThreadPoolExecutor(max_workers=8)
+        self._process_executor = ProcessPoolExecutor(max_workers=2)
+
+    def register(self, event_type: type, handler):
+        emitter_obj = self._emitter.get(event_type)
+
+        if emitter_obj is None:
+            emitter_obj = Signal(event_type.__name__)
+            self._emitter[event_type] = emitter_obj
+
+        emitter_obj.connect(handler, weak=False)
+
+
+    def publish(self, events):
+        for event in events:
+            event_type = type(event)
+            emitter_obj = self._emitter.get(event_type)
+
+            if emitter_obj:
+                for receiver in emitter_obj.receivers_for(event):
+                    if inspect.iscoroutinefunction(receiver):
+                        asyncio.create_task(
+                            self._safe_async_handle(receiver, event)
+                        )
+                    elif receiver in cpu_bound_handlers:
+                        self._process_executor.submit(
+                            self._safe_handle,
+                            receiver,
+                            event
+                        )
+                    else:
+                        self._thread_executor.submit(
+                            self._safe_handle,
+                            receiver,
+                            event
+                        )
+
+
+    def _safe_handle(self, handler, event):
+        try:
+            handler(event)
+        except Exception:
+            self._logger.exception(
+                "Error handling event %s",
+                type(event).__name__
+            )
+    async def _safe_async_handle(self, handler, event):
+        try:
+            await handler(event)
         except Exception:
             self._logger.exception(
                 "Error handling event %s",
